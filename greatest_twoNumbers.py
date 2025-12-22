47a9757cf7460bc4547abfda174b0d70eac57ea9
Sathya K <149158391+asanm11711721214009@users.noreply.github.com>
2025-12-22 17:17:15 +0530

    Add files via upload

---
+++ greatest_twoNumbers.py
@@ -0,0 +1,17 @@
+#using if_else statement
+# ternary operator
+#inbuilt_max function
+
+#1.
+a=30
+b=50
+if a>b:
+    print("gratest number are 30")
+else:
+    print("gratest number are 50")
+
+#2.
+print("greatest number are 30" if a>b else"greatest number are 50")
+
+#3.
+print(max(a,b))
