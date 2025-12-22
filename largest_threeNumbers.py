9200b008e4f4edb83bb6c1d42e15568a69d22d05
Sathya K <149158391+asanm11711721214009@users.noreply.github.com>
2025-12-22 18:16:02 +0530

    Add files via upload

---
+++ largest_threeNumbers.py
@@ -0,0 +1,21 @@
+# using if-else statement
+a=60
+b=90
+c=30
+if a>=b and a>=c:
+    print(a)
+elif b>=c and b>=a:
+    print(b)
+else:
+    print(c)
+
+#2.user input
+d=int(input("enter the number:"))
+e=int(input("enter the number:"))
+f=int(input("enter the number:"))
+if d>=e and d>=f:
+    print(d)
+elif e>=f and e>=d:
+    print(e)
+else:
+    print(f)
