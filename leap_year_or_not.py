0672d8cfec0a3e871cf27f5e127660031bfa12a2
Sathya K <149158391+asanm11711721214009@users.noreply.github.com>
2025-12-22 19:58:45 +0530

    Update leap_year_or_not.py

--- leap_year_or_not.py
+++ leap_year_or_not.py
@@ -14,6 +14,15 @@ if m%4==0 and m%100!=0 or m%400==0:
 else:
     print("not leap year")
 
+#3 
+import calendar
+m=int(input("Enter the year:")
+def is_leap_year(year)
+    return calendar.is leap(year)
+print(f"{year} is a leap year:")    
+    
+
+
 
 
 
